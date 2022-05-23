ff0ca00a41fbd6791c564d5b359a09808f70177d
rigol <rigolet.antoine@gmail.com>
2022-05-23 11:13:27 +0200

    modif classe role

--- src/main/java/com/inti/SpringBootValidationThymeleafA/model/Role.java
+++ src/main/java/com/inti/SpringBootValidationThymeleafA/model/Role.java
@@ -1,6 +1,23 @@
 package com.inti.SpringBootValidationThymeleafA.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table
 public class Role
 {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nomRole;
 }
