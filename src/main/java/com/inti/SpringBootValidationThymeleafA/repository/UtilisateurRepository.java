2c63adf05d7f1f87dca1de138802596699e1d417
rigol <rigolet.antoine@gmail.com>
2022-05-23 10:46:15 +0200

    Ajout de l'interface UtilisateurRepository

--- src/main/java/com/inti/SpringBootValidationThymeleafA/repository/UtilisateurRepository.java
+++ src/main/java/com/inti/SpringBootValidationThymeleafA/repository/UtilisateurRepository.java
@@ -3,10 +3,11 @@ package com.inti.SpringBootValidationThymeleafA.repository;
 import com.inti.SpringBootValidationThymeleafA.model.Utilisateur;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer>
+public interface UtilisateurRepository extends JpaRepository<User, Integer>
 {
 
 }
