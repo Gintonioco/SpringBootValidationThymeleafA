b9746a31c7dfcd6282b2bdb8194aa04a29456f8a
rigol <rigolet.antoine@gmail.com>
2022-05-23 10:46:35 +0200

    Revert "Ajout de l'interface UtilisateurRepository"
    This reverts commit 2c63adf05d7f1f87dca1de138802596699e1d417.

--- src/main/java/com/inti/SpringBootValidationThymeleafA/repository/UtilisateurRepository.java
+++ src/main/java/com/inti/SpringBootValidationThymeleafA/repository/UtilisateurRepository.java
@@ -3,11 +3,10 @@ package com.inti.SpringBootValidationThymeleafA.repository;
 import com.inti.SpringBootValidationThymeleafA.model.Utilisateur;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UtilisateurRepository extends JpaRepository<User, Integer>
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer>
 {
 
 }
